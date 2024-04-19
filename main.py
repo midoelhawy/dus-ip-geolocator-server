c963175df3f0edb0175b2df6d80136200fe10b88
Ahmed Hekal <ahekal@movia.biz>
2024-04-19 13:27:46 +0200

    improve: use waitressas http server

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from flask import Flask, request, jsonify
+from waitress import serve
+import logging
+import sys
+from functools import wraps
 
 from lib.db import DBHandler
 
 app = Flask(__name__)
+db = DBHandler()
 
 
-db =  DBHandler()
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
+def log_requests(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        response = func(*args, **kwargs)
+        status_code = response[1] if isinstance(response, tuple) else 200
+        logging.info(f"Received {request.method} request for {request.path} from {request.remote_addr}, Status: {status_code}")
+        return response
+    return wrapper
 
 @app.route('/geolocate/<ip>', methods=['GET'])
+@log_requests
 def geolocate_single_ip(ip):
     try:
         result = db.resolve_ip(ip)
@@ -20,6 +34,7 @@ def geolocate_single_ip(ip):
         return jsonify({'error': str(e)}), 400
 
 @app.route('/geolocate', methods=['POST'])
+@log_requests
 def geolocate_multiple_ips():
     try:
         data = request.json
@@ -29,4 +44,4 @@ def geolocate_multiple_ips():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    serve(app, host='0.0.0.0', port=5000)
